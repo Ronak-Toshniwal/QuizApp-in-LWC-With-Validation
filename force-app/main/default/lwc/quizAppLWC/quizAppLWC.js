3b8866a16ce3ef11eb82fc2353f5554b09b356a4
Ronak Toshniwal <73140231+Ronak-Toshniwal@users.noreply.github.com>
2022-05-08 12:23:11 +0530

    fixed few bugs in navigation

--- force-app/main/default/lwc/quizAppLWC/quizAppLWC.js
+++ force-app/main/default/lwc/quizAppLWC/quizAppLWC.js
@@ -1,10 +1,12 @@
-import { api,  LightningElement } from 'lwc';
+import { api,  LightningElement, track } from 'lwc';
 
 export default class QuizAppLWC extends LightningElement {
-     showQuestions = true;
+    showQuestions = true;
+    @track
     questions =[
     {
         id : 'input-1',
+        show : true,
         'q':'An administrator at Universal Container needs an automated way to delete records based on field values. What automated solution should the administrator use??',
         'a':[   {label: 'Workflow',value: 'option1'},
                 {label: 'Process Builder',value: 'option2'},
@@ -15,6 +17,7 @@ export default class QuizAppLWC extends LightningElement {
     },
     {
         id : 'input-2',
+        show : false,
         'q':'The Human resources department at Northern Trail outfitters wants employees to provide feedback about the manager using a custom object in Salesforce. It is important that managers are unable to see the feedback records from their staff. How should an administrator configure the custom object to meet this requirement??',
         'a':[   {label: 'Uncheck grant access using Hierarchies.',value: 'option1'},
                 {label: 'Define a criteria-based sharing rules.',value: 'option2'},
@@ -24,6 +27,7 @@ export default class QuizAppLWC extends LightningElement {
     },
     {
         id : 'input-3',
+        show : false,
         'q':'The VP of Sales at Cloud Kicks is receiving an error message that prevents them form saving an opportunity. The administrator attempted the same edit without receiving an error. How can the administrator validate the error the user is receiving?',
         'a':[   {label: 'Edit the page layout.',value: 'option1'},
                 {label: 'View the setup audit trail.',value: 'option2'},
@@ -33,6 +37,7 @@ export default class QuizAppLWC extends LightningElement {
     },
     {
         id : 'input-4',
+        show : false,
         'q':'The administrator at Aw Computing wants Account Details, related list and chatter feeds to each appear on separate tabs when reviewing an account. Which type of page should the administrator create?',
         'a':[   {label: 'Lightning app page.',value: 'option1'},
                 {label: 'Lightning page Tab.',value: 'option2'},
@@ -42,6 +47,7 @@ export default class QuizAppLWC extends LightningElement {
     },
     {
         id : 'input-5',
+        show : false,
         'q':'Cloud kicks want to give credit to Opportunity team members based on the level of effort contributed by each person toward each deal. What feature should the administrator use to meet this requirement?',
         'a':[   {label: 'Stages',value: 'option1'},
                 {label: 'Splits',value: 'option2'},
@@ -51,6 +57,7 @@ export default class QuizAppLWC extends LightningElement {
     },
     {
         id : 'input-6',
+        show : false,
         'q':'Northern Trail Outfitters has a new flow that automatically sets the field values when a new account is created. That the flow is launched by a process, But the flow is not working properly. What should administrator do to identify the problem?',
         'a':[   {label: 'Use the native debug feature in the flow builder.',value: 'option1'},
                 {label: 'Review debug logs with the login level.',value: 'option2'},
@@ -60,6 +67,7 @@ export default class QuizAppLWC extends LightningElement {
     },
     {
         id : 'input-7',
+        show : false,
         'q':'Ursa Major Solar offers amazing experiences for all of it employees. The Employee engagement committee wants to post updates while restricting other employees from posting. What should the administrator create to meet this request?',
         'a':[   {label: 'Chatter Stream.',value: 'option1'},
                 {label: 'Chatter Broadcast Group.',value: 'option2'},
@@ -69,6 +77,7 @@ export default class QuizAppLWC extends LightningElement {
     },
     {
         id : 'input-8',
+        show : false,
         'q':'The sales manager at cloud Kicks approves time off for their employees. They asked the administrator to ensure these requests are seen and responded to by a backup manager while the sales manager is out on vacation. What should administrator use to fulfill the requirement?',
         'a':[{label: 'Delegated approver',value: 'option1'},
                 {label: 'Two step Approval process',value: 'option2'},
@@ -78,6 +87,7 @@ export default class QuizAppLWC extends LightningElement {
     },
     {
         id : 'input-9',
+        show : false,
         'q':'Cloud Kicks has a custom object called Shipments. The Company wants to see all the shipment items from an Account page. When an Account is deleted, the shipments should remain. What type of relationship should the administrator make between Shipments and Account?',
         'a':[   {label: 'Shipments should have a lookup to Account.',value: 'option1'},
                 {label: 'Accounts should have a lookup to Shipments.',value: 'option2'},
@@ -87,6 +97,7 @@ export default class QuizAppLWC extends LightningElement {
     },
     {
         id : 'input-10',
+        show : false,
         'q':'A sales rep at Ursa Major Solar has launched a series of networking events. They are hosting one event per month and want to be able to report on campaign ROI by month and series. How should the administrator set up the Campaign to simplify reporting?',
         'a':[   {label: 'Add different record types for the monthly event types.',value: 'option1'},
                 {label: 'Create individual Campaigns that all have the same name.',value: 'option2'},
@@ -100,17 +111,16 @@ export default class QuizAppLWC extends LightningElement {
     questionsWithAns = new Map();
     @api score=0;
 
-    renderedCallback() {
+    connectedCallback() {
+        console.log('connectedCallBack q : ' , this.questions)
         try {
             this.questions.forEach(x => this.questionsWithAns.set(x.id, x.value))
           }
           catch(err) {
             console.error(err)
           }
-    }
-    
-
-
+          
+}
     handleChange(event){
       
         this.storeAnswers.set(event.target.name , event.target.value);
@@ -118,6 +128,10 @@ export default class QuizAppLWC extends LightningElement {
     }
     submitClick(){
         console.log('clicked')
+        console.log('submitClick q ' , this.questions);
+        console.log('qWa :' , this.questionsWithAns)
+        console.log('storeAns' , this.answers)
+        
         if(this.storeAnswers.size === this.questionsWithAns.size){
             this.errorMsg=false;
             try{
@@ -126,12 +140,10 @@ export default class QuizAppLWC extends LightningElement {
                     if(this.storeAnswers.get(key) === this.questionsWithAns.get(key)){
                         console.log('correct' ,key)
                         this.score++;
-                       // document.getElementById(this.storeAnswers.get(key)).style.color="#00ff00";
                     } else {
                         console.log('wrong' , key)
                     }
                 }
-               
              } catch(err){
                  console.log('err' ,err)
              }
@@ -143,15 +155,76 @@ export default class QuizAppLWC extends LightningElement {
                     this.errorMsg=true;
                    this.template.querySelector(`div[class=${key}]`).style.border = "thin solid red";
                     console.log('key', key)
+                } else {
+                    this.template.querySelector(`div[class=${key}]`).style.border = "";
                 }
             }
             
         }
-       
-        
         console.log('ques ', this.questionsWithAns)
         console.log('ans ',this.storeAnswers)
        console.log('score : ' + this.score +' out of ' + this.questions.length)
        
+    }
+
+    currentPage = 0 ;
+    totalPages = this.questions.length-1;
+    lastBtn = false;
+    
+    nextHandler(){
+        if(this.currentPage < this.totalPages){
+            console.log(this.currentPage++);
+            console.log(this.questions[this.currentPage]);
+            this.questions[this.currentPage].show = true;
+            this.questions[this.currentPage-1].show = false;
+            console.log(this.questions);
+        }
+        if(this.currentPage === this.totalPages){
+            this.lastBtn = true;
+        } else {
+            this.lastBtn = false;
+        }
+    }
+
+    previousHandler(){
+       if(this.currentPage > 0 ){
+             this.questions[this.currentPage].show = false;
+             console.log(this.currentPage--);
+            this.questions[this.currentPage].show = true;
+            
+            console.log(this.questions[this.currentPage]);
+
+            
+        }
+        if(this.currentPage === this.totalPages){
+            this.lastBtn = true;
+        } else {
+            this.lastBtn = false;
+        }
+    }
+
+    firstPageHandler(){
+        this.currentPage = 0;
+        console.log(this.currentPage);
+        console.log(this.questions[this.currentPage]);
+        if(this.currentPage === this.totalPages){
+            this.lastBtn = true;
+        } else {
+            this.lastBtn = false;
+        }
+    }
+
+    lastPageHandler(){
+            this.questions[this.currentPage].show = false;
+            this.currentPage = this.totalPages - 1;
+            this.questions[this.currentPage].show = true;
+        
+        console.log(this.currentPage);
+        console.log(this.questions[this.currentPage]);
+        if(this.currentPage === this.totalPages-1){
+            this.lastBtn = true;
+        } else {
+            this.lastBtn = false;
+        }
     }
 }
